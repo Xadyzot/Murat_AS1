e5aec6761e40b8e008fc681a344c33365ba6cd37
Xadyzot <xadyzot@gmail.com>
2025-03-31 22:55:07 +0500

    0.0.2

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class PostController extends Controller
+{
+    public function index() {
+        return 'posts.index';
+    }
+}
