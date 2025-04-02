bd98301490a32cef1d0ebf7337340c98b431d814
Xadyzot <xadyzot@gmail.com>
2025-04-02 09:33:10 +0500

    1.0.4

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
         $this->call(CitySeeder::class);
+        $this->call(RestaurantSeeder::class);
+        $this->call(CategorySeeder::class);
+
 
     }
 }
